41f837a7e1456ae7d2556088b362fd17921adf63
Aditya-Paly <aoberai05@gmail.com>
2020-06-25 15:20:17 -0700

    transferring from other repo

---
+++ src/main/java/com/palyrobotics/config/VisionConfig.java
@@ -0,0 +1,7 @@
+package com.palyrobotics.config;
+
+public class VisionConfig extends AbstractConfig {
+
+	public int captureWidth = 320, captureHeight = 240, captureFps = 30;
+	public boolean showImage = true;
+}
